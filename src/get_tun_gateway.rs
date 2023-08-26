0a86cbf7592bd7207bbae7ab89407193ce88e50c
apidullah <inceabdullah@yahoo.com>
2023-08-26 19:30:21 +0300

    route dbg

--- src/get_tun_gateway.rs
+++ src/get_tun_gateway.rs
@@ -4,8 +4,8 @@ use crate::interface_utils::get_interface_name_from_index;  // Import the helper
 pub async fn get(tun_device_name: &str) -> std::io::Result<Option<String>> {
     let handle = Handle::new()?;
     let routes = handle.list().await?;
-    println!("routes: {:?}", routes);
     for route in routes {
+        println!("route: {:?}", route);
         if let Some(ifindex) = route.ifindex {
             let interface_name = get_interface_name_from_index(ifindex).unwrap_or_default();  // Use the helper function
 
