0870042bc5f4d32042d2489dec048b86adfa17b1
apidullah <inceabdullah@yahoo.com>
2023-08-31 16:54:44 +0300

    add add_clean_masq_for_def

--- src/nft_utils/mod.rs
+++ src/nft_utils/mod.rs
@@ -1,3 +1,13 @@
+use crate::route_utils;
+
 pub mod masq;
 
+pub async fn add_clean_masq_for_def() {
+    let ifname = route_utils::def::get_iface_name().await;
+    println!("cleaning... {} masq", &ifname);
+    nft_utils::masq::rm_masq_for_ifname(&ifname);
+    println!("adding... {} masq", &ifname);
+    nft_utils::masq::add_masq_for_ifname(&ifname);
 
+    println!("Added {} masq", &ifname);
+}
